8bd58aaa52f702066f27b05c05fceec3555ab5b0
Angel Javier <angel_java@hotmail.com>
2023-04-12 12:48:05 -0600

    First Java Loop Challenge

--- src/main/java/com/hackerrank/java/common/JavaLoopOne.java
+++ src/main/java/com/hackerrank/java/common/JavaLoopOne.java
@@ -1,5 +1,19 @@
 package com.hackerrank.java.common;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class JavaLoopOne {
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+		for (int i = 1; i <= 10; i++) {
+			int result = n * i;
+			System.out.println(n + " x " + i + " = " + result);
+		}
+		bufferedReader.close();
+	}
 }
