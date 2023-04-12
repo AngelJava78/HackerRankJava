dbdeae8d539d1f5ef8410bbcec8da0c3fed9e1df
Angel Javier <angel_java@hotmail.com>
2023-04-12 10:25:11 -0600

    Scanner challenge was added to the project

---
+++ src/main/java/com/hackerrank/java/stdinstdout/ScannerImplement.java
@@ -0,0 +1,19 @@
+package com.hackerrank.java.stdinstdout;
+
+import java.util.Scanner;
+
+public class ScannerImplement {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // Write your code here.
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        
+    }
+}
